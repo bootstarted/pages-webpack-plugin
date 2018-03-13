00a6924a580d686344883fae9f9cb0b43ca87d2a
Neal Granger <neal@nealg.com>
2018-03-13 12:24:08 -0700

    WIP Generics rewrite

--- src/PagesPlugin.js
+++ src/PagesPlugin.js
@@ -1,105 +1,71 @@
 // @flow
+
 import cheerio from 'cheerio';
 import loaderUtils from 'loader-utils';
+import path from 'path';
 
 type RenderResult = {
   markup: string,
+  redirect?: string,
+  status?: number,
 };
 
-type OutputResult = {
-  markup: string,
+type RenderProps = {
   path: string,
 };
 
-type RenderFunction = (props: Object) => RenderResult | Promise<RenderResult>;
+type OutputResult<T: RenderResult> = {
+  ...T,
+  markup: string,
+  path: string,
+  filename: string,
+};
 
-type Options = {
+type RequiredOptions<T: RenderResult, P: RenderProps> = {|
+  mapStatsToProps: (stats: Object) => P,
+  render: (props: P) => T | Promise<T>,
+|};
+
+type Options<T: RenderResult, P: RenderProps> = {
+  ...RequiredOptions<T, P>,
   name: string,
-  directory?: ((OutputResult) => boolean) | boolean,
-  mapStatsToProps: (stats: Object) => Object,
-  mapResults?: (Array<OutputResult>) => Array<OutputResult>,
-  render: RenderFunction,
-  paths?: Array<string>,
-};
-
-type RenderPagesOptions = {
-  render: RenderFunction,
-  props: Object,
   paths: Array<string>,
+  directory: boolean | ((OutputResult<T>) => boolean),
+  mapResults: (Array<OutputResult<T>>) => Array<OutputResult<T>>,
 };
 
-const renderPages = ({render, props, paths = ['/']}: RenderPagesOptions) => {
-  const pathMap = {};
-  const renderPaths = (
-    results: Array<OutputResult>,
-    paths: Array<string>
-  ): Promise<Array<OutputResult>> => {
-    if (paths.length === 0) {
-      return Promise.resolve(results);
-    }
-    const next = [];
-    const newResults = results.slice();
-    return paths
-      .reduce((previous: Promise<*>, path: string) => {
-        if (pathMap[path]) {
-          return previous;
-        }
-        pathMap[path] = true;
-        return previous.then(() => {
-          const result: Promise<RenderResult> = Promise.resolve(
-            render({path, ...props})
-          );
-          return result.then((data) => {
-            // TODO: ^ Check `statusCode`, `redirect` ?? etc. and not generate
-            // pages that have e.g. 404 or 500 errors.
-            newResults.push({path, ...data});
-            const $ = cheerio.load(data.markup);
-            $('a[href^="/"]').each((i, elem) => {
-              const path = $(elem).attr('href');
-              next.push(path);
-            });
-          });
-        });
-      }, Promise.resolve())
-      .then(() => {
-        return renderPaths(newResults, next);
-      });
-  };
-  return renderPaths([], paths);
-};
+class PagesPlugin<T: RenderResult, P: RenderProps> {
+  options: Options<T, P>;
 
-const defaultOptions: Options = {
-  name: '[path][name].[ext]',
-  paths: ['/'],
-  mapStatsToProps: () => {
-    throw new TypeError('Must pass valid `mapStatsToProps` option.');
-  },
-  render: () => {
-    throw new TypeError('Must pass valid `render` option.');
-  },
-  directory: (result: OutputResult): boolean => {
-    const parts = result.path.split('/');
-    return parts[parts.length - 1].indexOf('.') < 0;
-  },
-};
-
-class PagesPlugin {
-  options: Options;
-
-  constructor(options: Options) {
+  constructor(options: RequiredOptions<T, P>) {
     this.options = {
-      ...defaultOptions,
+      name: '[path][name].[ext]',
+      paths: ['/'],
+      directory: (result) => path.extname(result.path) === '',
+      mapResults: (results) => results,
       ...options,
     };
+
+    if (typeof this.options.mapStatsToProps !== 'function') {
+      throw new TypeError(
+        'PagesPlugin options must contain `mapStatsToProps` function.',
+      );
+    }
+
+    if (typeof this.options.render !== 'function') {
+      throw new TypeError(
+        'PagesPlugin options must contain `render` function.',
+      );
+    }
   }
 
-  getName(resourcePath: string, options: Object) {
+  getFilename(resourcePath: string, options: Object) {
     return loaderUtils
       .interpolateName({resourcePath}, this.options.name, options)
       .replace(/^\.\//, '');
   }
 
-  getUseDirectory(result: OutputResult): boolean {
+  getUseDirectory(result: OutputResult<T>): boolean {
     if (typeof this.options.directory === 'boolean') {
       return this.options.directory;
     } else if (typeof this.options.directory === 'function') {
@@ -108,50 +74,91 @@ class PagesPlugin {
     return true;
   }
 
-  normalizePath(result: OutputResult) {
+  normalizePath(result: OutputResult<T>) {
     if (result.path.charAt(0) !== '/') {
-      throw new TypeError();
+      throw new TypeError(
+        `PagesPlugin encountered invalid path: ${result.path}`,
+      );
     }
+
     const path = this.getUseDirectory(result)
       ? `${result.path.substr(1)}/index.html`
       : result.path.substr(1);
+
     return path.replace(/^\//, './').replace(/\/\//g, '/');
   }
 
-  apply(compiler: any) {
-    const {mapStatsToProps, render, paths} = this.options;
-    compiler.plugin('emit', (compilation, callback) => {
-      const stats = compilation.getStats().toJson();
-      renderPages({
-        render,
-        props: mapStatsToProps(stats),
-        paths: paths || ['/'],
-      })
-        .then((preResults) => {
-          preResults.forEach((result) => {
-            const path = this.getName(this.normalizePath(result), {
-              content: result.markup,
-            });
-            result.path = path;
-          });
-          const results =
-            typeof this.options.mapResults === 'undefined'
-              ? preResults
-              : this.options.mapResults(preResults);
-          results.forEach((result) => {
-            compilation.assets[result.path] = {
-              source: function() {
-                return result.markup;
-              },
-              size: function() {
-                return result.markup.length;
-              },
-            };
-          });
-          callback();
-        })
-        .catch(callback);
+  parsePathsFromMarkup(markup: string): Array<string> {
+    const $ = cheerio.load(markup);
+    const relativeLinks = $('a[href^="/"], a[href^="."]');
+
+    const paths = [];
+
+    relativeLinks.each((i, element) => {
+      paths.push($(element).attr('href'));
     });
+
+    return paths;
+  }
+
+  renderPages(
+    props: P,
+    paths: Array<string>,
+    renderedPaths: Array<string> = [],
+    results: Array<OutputResult<T>> = [],
+  ): Promise<Array<OutputResult<T>>> {
+    if (paths.length === 0) return Promise.resolve(results);
+
+    let discoveredPaths = [];
+    return paths
+      .reduce((previous, path) => {
+        if (renderedPaths.includes(path)) return previous;
+
+        renderedPaths.push(path);
+
+        return previous
+          .then(() => this.options.render({...props, path}))
+          .then((result) => {
+            // TODO: ^ Check `statusCode`, `redirect` ?? etc. and not generate
+            // pages that have e.g. 404 or 500 errors.
+            results.push({...result, filename: '', path});
+            discoveredPaths = discoveredPaths.concat(
+              this.parsePathsFromMarkup(result.markup)
+            );
+          });
+      }, Promise.resolve())
+      .then(() => {
+        return this.renderPages(props, discoveredPaths, renderedPaths, results);
+      });
+  }
+
+  handleEmit = (compilation: Object, done: (?Error) => void) => {
+    const stats = compilation.getStats().toJson();
+
+    this.renderPages(this.options.mapStatsToProps(stats), this.options.paths)
+      .then((results) =>
+        results.map((result) => ({
+          ...result,
+          filename: this.getFilename(this.normalizePath(result), {
+            content: result.markup,
+          }),
+        })),
+      )
+      .then(this.options.mapResults)
+      .then((results) => {
+        results.forEach((result) => {
+          compilation.assets[result.filename] = {
+            source: () => result.markup,
+            size: () => result.markup.length,
+          };
+        });
+        done();
+      })
+      .catch(done);
+  };
+
+  apply(compiler: Object) {
+    compiler.plugin('emit', this.handleEmit);
   }
 }
 
